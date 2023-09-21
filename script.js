1dca145b2c8310565b65dbc399eca84f9053ce2e
wakeem <134740381+hdsnisnotreal@users.noreply.github.com>
2023-09-21 11:53:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const notesList = document.getElementById('notes-list');
-    const noteText = document.getElementById('note-text');
-    const addNoteButton = document.getElementById('add-note');
-    const clearNotesButton = document.getElementById('clear-notes');
+const noteContent = document.getElementById("note-content");
 
-    addNoteButton.addEventListener('click', function () {
-        const noteContent = noteText.value.trim();
-        if (noteContent !== '') {
-            const li = document.createElement('li');
-            li.textContent = noteContent;
-            notesList.appendChild(li);
-            noteText.value = '';
-        }
-    });
+// Load saved note from local storage on page load
+window.addEventListener("load", () => {
+    const savedNote = localStorage.getItem("savedNote");
+    if (savedNote) {
+        noteContent.value = savedNote;
+    }
+});
 
-    clearNotesButton.addEventListener('click', function () {
-        notesList.innerHTML = '';
-    });
+// Save note to local storage when content changes
+noteContent.addEventListener("input", () => {
+    const content = noteContent.value;
+    localStorage.setItem("savedNote", content);
+});
+
+// Save note to local storage when the tab is closed or refreshed
+window.addEventListener("beforeunload", () => {
+    const content = noteContent.value;
+    localStorage.setItem("savedNote", content);
 });
