bf48685cc5da89c7c85cb1de5ddbee88a70ab541
wakeem <134740381+hdsnisnotreal@users.noreply.github.com>
2023-09-21 11:59:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
-const noteContent = document.getElementById("note-content");
+document.addEventListener('DOMContentLoaded', function () {
+    const notesList = document.getElementById('notes-list');
+    const noteInput = document.getElementById('note-input');
+    const saveButton = document.getElementById('save-button');
+    const updateMessage = document.getElementById('update-message');
+    
+    // Load saved notes from local storage
+    const savedNotes = JSON.parse(localStorage.getItem('notes')) || [];
 
-// Load saved note from local storage on page load
-window.addEventListener("load", () => {
-    const savedNote = localStorage.getItem("savedNote");
-    if (savedNote) {
-        noteContent.value = savedNote;
+    // Display saved notes
+    function displayNotes() {
+        notesList.innerHTML = '';
+        savedNotes.forEach((note, index) => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `
+                <span>${note}</span>
+                <button class="delete-button" data-index="${index}">Delete</button>
+            `;
+            notesList.appendChild(listItem);
+        });
     }
-});
 
-// Save note to local storage when content changes
-noteContent.addEventListener("input", () => {
-    const content = noteContent.value;
-    localStorage.setItem("savedNote", content);
-});
+    // Save a new note
+    saveButton.addEventListener('click', function () {
+        const noteText = noteInput.value.trim();
+        if (noteText !== '') {
+            savedNotes.push(noteText);
+            localStorage.setItem('notes', JSON.stringify(savedNotes));
+            noteInput.value = '';
+            displayNotes();
+        }
+    });
 
-// Save note to local storage when the tab is closed or refreshed
-window.addEventListener("beforeunload", () => {
-    const content = noteContent.value;
-    localStorage.setItem("savedNote", content);
+    // Delete a note
+    notesList.addEventListener('click', function (event) {
+        if (event.target.classList.contains('delete-button')) {
+            const index = event.target.getAttribute('data-index');
+            savedNotes.splice(index, 1);
+            localStorage.setItem('notes', JSON.stringify(savedNotes));
+            displayNotes();
+        }
+    });
+
+    // Display update message
+    function displayUpdateMessage() {
+        updateMessage.textContent = 'Site updated!';
+        setTimeout(() => {
+            updateMessage.textContent = 'No updates yet.';
+        }, 5000);
+    }
+
+    // Initially display notes and set up update message
+    displayNotes();
+    displayUpdateMessage();
 });
